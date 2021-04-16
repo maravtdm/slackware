7bb6bef63cbbae96e8c3a12749a7a8d11fb8f9c1
marav <11086888+maravtdm@users.noreply.github.com>
2021-04-16 15:11:57 +0200

    Update lookandfeel.sh

--- lookandfeel.sh
+++ lookandfeel.sh
@@ -10,10 +10,10 @@ DAY=$(date "+%d %b %Y")
 CITY=30799
 curl https://www.lecalendrier.fr/calendrier-solaire\?city\=${CITY} > $HOME/.bin/city.out
 
-START=$(cat $HOME/.bin/city.out | grep "${DAY}" | tail -n2 | cut -d">" -f5 | cut -d"<" -f1 | sed 's/h//' | head -1)
-END=$(cat $HOME/.bin/city.out | grep "${DAY}" | tail -n2 | cut -d">" -f7 | cut -d"<" -f1 | sed 's/h//' | head -1)
-
 while [ "${DATE}" -lt "2359" ] ; do
+  START=$(cat $HOME/.bin/city.out | grep "${DAY}" | tail -n2 | cut -d">" -f5 | cut -d"<" -f1 | sed 's/h//' | head -1)
+  END=$(cat $HOME/.bin/city.out | grep "${DAY}" | tail -n2 | cut -d">" -f7 | cut -d"<" -f1 | sed 's/h//' | head -1)
+
   if [ "${DATE}" -lt "${END}" ] && [ "${DATE}" -gt "${START}" ] ; then
     if [ "${LOOK}" != "org.kde.breezetwilight.desktop" ] ; then
       lookandfeeltool --apply org.kde.breezetwilight.desktop
