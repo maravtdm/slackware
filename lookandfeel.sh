0d0bacd0bd4e951db97ee9ab8e63f46b43820bd2
marav <11086888+maravtdm@users.noreply.github.com>
2021-04-16 13:48:26 +0200

    Update lookandfeel.sh

--- lookandfeel.sh
+++ lookandfeel.sh
@@ -2,9 +2,6 @@
 ### Change plasma5 look between 9AM and 7PM (timezone CEST)
 ### Switch : Twilight theme <> Dark theme
 
-LOOK=$(cat $HOME/.config/kdeglobals | awk -F"=" '/LookAndFeelPackage/ {print $2}')
-DATE=$(date +%H%M)
-
 # Modify this according to your needs (hhmm)
 START="0900"
 END="1900"
@@ -13,16 +10,18 @@ while [ "${DATE}" -lt "2359" ] ; do
   if [ "${DATE}" -lt "${END}" ] && [ "${DATE}" -gt "${START}" ] ; then
     if [ "${LOOK}" != "org.kde.breezetwilight.desktop" ] ; then
       lookandfeeltool --apply org.kde.breezetwilight.desktop
-      notify-send "Twilight Theme : On"
+      notify-send "Look&Feel" "Twilight Theme : On"
     fi
   else 
     if [ "${LOOK}" != "org.kde.breezedark.desktop" ] ; then
       lookandfeeltool --apply org.kde.breezedark.desktop
-      notify-send "Dark Theme : On"
+      notify-send "Look&Feel" "Dark Theme : On"
     fi
   fi
   LOOK=$(cat $HOME/.config/kdeglobals | awk -F"=" '/LookAndFeelPackage/ {print $2}')
   DATE=$(date +%H%M)
-  sleep 10m
+  sleep 5s
 done
+
+
 
