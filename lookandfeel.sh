e3759fb26b7d10ed1bb98efea756e8203b1903b9
marav <11086888+maravtdm@users.noreply.github.com>
2021-04-21 22:26:25 +0200

    Update lookandfeel.sh

--- lookandfeel.sh
+++ lookandfeel.sh
@@ -5,30 +5,40 @@
 ### Autostart with session :
 ### Add the script to : System settings / Startup and Shutdown / autostart
 
+# Log everything
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>$HOME/.bin/lookandfeel.log 2>&1
+echo `date`>$HOME/.bin/lookandfeel.log
+set -x
+
 LOOK=$(cat $HOME/.config/kdeglobals | awk -F"=" '/LookAndFeelPackage/ {print $2}')
 DATE=$(date +%H%M)
 DAY=$(date "+%d %b %Y")
 
-# Wait for the network
+# Wait for the network, specialy with wireless connection
 sleep 10s 
-# go to https://www.lecalendrier.fr/calendrier-solaire and choose your city, and note the zip code a the end of url
-# Zip code modifications requiert a log out/in 
-# Example, for Paris/France
+
+# Modify this according to your needs (hhmm)
+# Or go to https://www.lecalendrier.fr/calendrier-solaire and choose your city, and note the zip code a the end of url
 CITY=30799
-curl https://www.lecalendrier.fr/calendrier-solaire\?city=${CITY} > $HOME/.bin/city.out
+curl https://www.lecalendrier.fr/calendrier-solaire?city=${CITY} > $HOME/.bin/city.out
 
 while [ "${DATE}" -lt "2359" ] ; do
-  START=$(cat $HOME/.bin/city.out | grep "${DAY}" | tail -n2 | cut -d">" -f5 | cut -d"<" -f1 | sed 's/h//' | head -1)
-  END=$(cat $HOME/.bin/city.out | grep "${DAY}" | tail -n2 | cut -d">" -f7 | cut -d"<" -f1 | sed 's/h//' | head -1)
+START=$(cat $HOME/.bin/city.out | grep "${DAY}" | tail -n2 | cut -d">" -f5 | cut -d"<" -f1 | sed 's/h//' | head -1)
+END=$(cat $HOME/.bin/city.out | grep "${DAY}" | tail -n2 | cut -d">" -f7 | cut -d"<" -f1 | sed 's/h//' | head -1)
+# Defaults values if url is unreachable or network is down
+START=${START:-0900}
+END=${END:-2130}
 
   if [ "${DATE}" -lt "${END}" ] && [ "${DATE}" -gt "${START}" ] ; then
     if [ "${LOOK}" != "org.kde.breezetwilight.desktop" ] ; then
-      lookandfeeltool --apply org.kde.breezetwilight.desktop
+      lookandfeeltool --apply org.kde.breezetwilight.desktop >/dev/null 2>&1
       notify-send "Look&Feel" "Twilight Theme : On"
     fi
   else 
     if [ "${LOOK}" != "org.kde.breezedark.desktop" ] ; then
-      lookandfeeltool --apply org.kde.breezedark.desktop
+      lookandfeeltool --apply org.kde.breezedark.desktop >/dev/null 2>&1
       notify-send "Look&Feel" "Dark Theme : On"
     fi
   fi
